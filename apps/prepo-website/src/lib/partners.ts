c07edad0238567905d495ee8128e4bcfae5b14e8
Roan <r0x4xn@protonmail.com>
2022-04-13 09:49:53 +0200

    fix: lib folder ignore prepo-sdk

---
+++ apps/prepo-website/src/lib/partners.ts
@@ -0,0 +1,412 @@
+import { Partner } from '../components/layout/InvestorsSection'
+
+export const investors: Partner[] = [
+  {
+    name: 'Republic Capital',
+    url: 'https://www.republiccapital.co/',
+    iconName: 'republicCapital',
+  },
+  {
+    name: 'IOSG Venture',
+    url: 'https://iosg.vc/',
+    iconName: 'iosgVenture',
+  },
+  {
+    name: 'Maven 11',
+    url: 'https://www.maven11.com/',
+    iconName: 'maven11',
+  },
+  {
+    name: 'The Lao',
+    url: 'https://www.thelao.io/',
+    iconName: 'theLao',
+  },
+  {
+    name: 'MEXC',
+    url: 'https://www.mexc.com/',
+    iconName: 'mexc',
+  },
+  {
+    name: 'AscendEX',
+    url: 'https://ascendex.com/',
+    iconName: 'ascendEX',
+  },
+  {
+    name: 'Shima Capital',
+    url: 'https://shima.capital/',
+    iconName: 'shimaCapital',
+  },
+  {
+    name: 'HoneyDAO',
+    url: 'https://www.honeydao.com/',
+    iconName: 'honeyDAO',
+  },
+  {
+    name: 'Apollo',
+    url: 'https://www.apollocap.io/',
+    iconName: 'apolloCapital',
+  },
+  {
+    name: 'Global Coin Research',
+    url: 'https://globalcoinresearch.com/',
+    iconName: 'gcr',
+  },
+  {
+    name: 'DCV',
+    url: 'https://twitter.com/dcv_capital',
+    iconName: 'dcv',
+  },
+  {
+    name: 'NeptuneDAO',
+    url: 'https://neptunedao.xyz/',
+    iconName: 'neptuneDAO',
+  },
+  {
+    name: 'Dexterity Capital',
+    url: 'https://www.dexterity.capital/',
+    iconName: 'dexterityCapital',
+  },
+  {
+    name: 'Flex Dapp',
+    url: 'https://flexdapps.com/',
+    iconName: 'flexDapps',
+  },
+  {
+    name: 'Koji',
+    url: 'https://koji.capital/',
+    iconName: 'koji',
+  },
+  {
+    name: 'Possible Ventures',
+    url: 'https://www.possible.ventures/',
+    iconName: 'possibleVentures',
+  },
+  {
+    name: 'CaballerosCapital',
+    url: 'https://caballeroscapital.com/',
+    iconName: 'caballerosCapital',
+  },
+  {
+    name: 'meldventures',
+    url: 'https://www.meld.ventures/',
+    iconName: 'meldVentures',
+  },
+  {
+    name: 'Fire Eyes',
+    url: 'http://fireeyes.xyz/',
+    iconName: 'fireEyes',
+  },
+  {
+    name: 'Berggruen Holdings',
+    url: 'https://www.berggruenholdings.com/',
+    iconName: 'berggruenHoldings',
+  },
+  {
+    name: 'Paperclip',
+    url: 'https://www.paperclip.partners/',
+    iconName: 'paperclip',
+  },
+  {
+    name: 'Re7 Capital',
+    url: 'https://www.linkedin.com/company/re7capital/ ',
+    iconName: 're7Capital',
+  },
+  {
+    name: 'NxGen',
+    url: 'https://www.nxgen.xyz/',
+    iconName: 'nxgen',
+  },
+  {
+    name: 'Fork Ventures',
+    url: 'https://www.forkholdings.com/',
+    iconName: 'forkVentures',
+  },
+]
+
+export const angels: Partner[] = [
+  {
+    name: 'Stefan George',
+    url: 'https://twitter.com/StefanDGeorge',
+    description: 'Gnosis',
+    imageUrl: 'https://pbs.twimg.com/profile_images/1162386851736543233/Zbiz-s07_400x400.jpg',
+  },
+  {
+    name: 'Anton Bukov',
+    url: 'https://twitter.com/k06a',
+    description: '1inch',
+    imageUrl: 'https://pbs.twimg.com/profile_images/1481298453661691904/FdP7AvEl_400x400.jpg',
+  },
+  {
+    name: 'Kieran Warwick',
+    url: 'https://twitter.com/KieranWarwick',
+    description: 'Illuvium',
+    iconName: 'illuvium',
+  },
+  {
+    name: 'Scoopy Trooples',
+    url: 'https://twitter.com/scupytrooples',
+    description: 'Alchemix',
+    iconName: 'alchemix',
+  },
+  {
+    name: 'Tyler Ward',
+    url: 'https://twitter.com/LordTylerWard',
+    description: 'BarnBridge, Universe.xyz',
+    imageUrl: 'https://pbs.twimg.com/profile_images/1298694547577462785/m_ZIw4kb_400x400.jpg',
+  },
+  {
+    name: 'Derek Yoo',
+    url: 'https://twitter.com/derekyoo',
+    description: 'Moonbeam',
+    imageUrl: 'https://pbs.twimg.com/profile_images/1254557815638822914/hsQWyLHZ_400x400.jpg',
+  },
+  {
+    name: 'Nodar Janashia',
+    url: 'https://twitter.com/NodarJ',
+    description: 'Zapper, DeFrag',
+    imageUrl: 'https://pbs.twimg.com/profile_images/1437890013149138946/H0vGRpyB_400x400.jpg',
+  },
+  {
+    name: 'Hilmar Orth',
+    url: 'https://twitter.com/hilmarxo',
+    description: 'Gelato',
+    imageUrl: 'https://pbs.twimg.com/profile_images/1382329326956384258/JVnEkS4F_400x400.jpg',
+  },
+  {
+    name: 'James Simpson',
+    url: 'https://twitter.com/0xApricity',
+    description: 'mStable',
+    iconName: 'mStable',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1468566979070070786/YcCaN4hQ_400x400.jpg',
+    name: 'Tristan Frizza',
+    url: 'https://twitter.com/Tristan0x',
+    description: 'Zeta',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1369201998319341569/htYowoY2_400x400.png',
+    name: 'Rudy Lee',
+    url: 'https://www.linkedin.com/in/rudyclee/',
+    description: 'Zepeto',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1371551541971324929/5YIOtk9N_400x400.jpg',
+    name: 'Mitchell Amador',
+    url: 'https://twitter.com/MitchellAmador',
+    description: 'Immunefi',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1480547758687899649/H20jU6-v_400x400.png',
+    name: 'Danijel',
+    url: 'https://twitter.com/veryHighLander',
+    description: 'Thales',
+  },
+  {
+    name: 'Agyle',
+    url: 'https://twitter.com/stillagyle',
+    description: 'Talisman',
+    iconName: 'talisman',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1002503691188912128/tfKbuEjQ_400x400.jpg',
+    name: 'Kyle Lu',
+    url: 'https://twitter.com/KyleJqlu',
+    description: 'Dapp.com',
+  },
+  {
+    name: 'Radek & Ermin',
+    url: 'https://twitter.com/radek1st',
+    description: 'dHedge',
+    iconName: 'dHedge',
+  },
+  {
+    name: 'Rob Salha',
+    url: 'https://www.linkedin.com/in/robsalha/',
+    description: 'Zed Run',
+    iconName: 'zedRun',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1349502130621714433/2LWlBjKo_400x400.jpg',
+    name: 'Harrison Hines',
+    url: 'https://twitter.com/harris0nhines',
+    description: 'Fleek',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1453133996804743173/1MLOKR3E_400x400.jpg',
+    name: 'Alex & Yaroslav',
+    url: 'https://twitter.com/AlexSmirnov__',
+    description: 'deBridge',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1372431023800119296/DrKjSQMH_400x400.png',
+    name: 'Ben Lakoff',
+    url: 'https://twitter.com/benlakoff',
+    description: 'Charged Particles',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1454912483248930822/_hO4WPRC_400x400.png',
+    name: 'Charlie Songhurst',
+    url: 'https://www.linkedin.com/in/charlessonghurst/',
+    description: 'Microsoft',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1106127755496714241/j5jZ6m2L_400x400.png',
+    name: 'See Eun Ha',
+    url: 'https://twitter.com/s3unha',
+    description: 'nonce, MetaCartel',
+  },
+  {
+    name: 'Matt Symons',
+    url: 'https://twitter.com/msymons',
+    description: 'SocietyOne',
+    iconName: 'societyOne',
+  },
+  {
+    imageUrl:
+      'https://static.wixstatic.com/media/c0d9b0_c98b42d1fc9e405386f1de48cc5d9e7b~mv2.png/v1/crop/x_65,y_78,w_231,h_220/fill/w_71,h_81,al_c,usm_0.66_1.00_0.01,enc_auto/c0d9b0_c98b42d1fc9e405386f1de48cc5d9e7b~mv2.png',
+    name: 'Ryon Nixon',
+    url: 'https://www.linkedin.com/in/ryonnixon/',
+    description: 'Horizons Law',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1465766402682494979/VNa78PIM_400x400.jpg',
+    name: 'Gorazd Ocvirk',
+    url: 'https://twitter.com/Gorazd_OV',
+    description: 'ENSO',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1468042901343162372/QWz9YlFR_400x400.png',
+    name: 'Ryan Chow',
+    url: 'https://twitter.com/RyanChow_DeFi',
+    description: 'Solv',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1084788308595617793/DOnqq1OM_400x400.jpg',
+    name: 'Wanseob Lim',
+    url: 'https://twitter.com/wanseoblim',
+    description: 'Ethereum Foundation, Zkopru',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1442917331093516290/iWV0Fp48_400x400.jpg',
+    name: 'state.eth',
+    url: 'https://twitter.com/statelayer',
+    description: 'Sudoswap',
+  },
+  {
+    imageUrl: 'https://www.larvalabs.com/cryptopunks/cryptopunk4544.png',
+    name: 'toast.eth',
+    url: 'https://twitter.com/intocryptoast',
+  },
+  {
+    iconName: 'octav',
+    name: 'Mathieu Baril',
+    url: 'https://twitter.com/mbaril010',
+    description: 'Octav',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1361317805384216579/4iIrKAKu_400x400.jpg',
+    name: '0xChop',
+    url: 'https://twitter.com/0xChop',
+    description: 'NFTX',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1445419155050704901/A0IGm5Lu_400x400.png',
+    name: 'Ryan Zurrer',
+    url: 'https://twitter.com/kukulabanze',
+    description: 'Dialectic',
+  },
+  {
+    imageUrl: 'https://pbs.twimg.com/profile_images/1437782087390662667/zb7ECWnq_400x400.jpg',
+    name: 'Auryn MacMillan',
+    url: 'https://twitter.com/auryn_macmillan',
+    description: 'Gnosis',
+  },
+  {
+    imageUrl: 'https://www.betashares.com.au/wp-content/themes/betashares/images/betashares.svg',
+    name: 'Alex Vynokur',
+    url: 'https://www.linkedin.com/in/vynokur/',
+    description: 'BetaShares',
+  },
+  {
+    name: 'Charles Vaughan',
+    description: 'Peter Thiel Capital',
+    iconName: 'thiel',
+  },
+]
+
+export const integrations: Partner[] = [
+  {
+    name: 'OpenSea',
+    url: 'https://opensea.io/collection/acquisition-royale',
+    iconName: 'openSea',
+  },
+  {
+    name: 'Polygon',
+    url: 'https://acquisitionroyale.com/',
+    iconName: 'polygon',
+  },
+  {
+    name: 'Uniswap',
+    url: 'https://url.prepo.io/RP-MATIC',
+    iconName: 'uniswap',
+  },
+  {
+    name: 'Sushi',
+    url: 'https://medium.com/sushiswap-org/introducing-the-sushi-next-generation-amm-trident-7dea6aa3cbc2',
+    iconName: 'sushi',
+  },
+  {
+    name: 'Dapp.com',
+    url: 'https://www.dapp.com/app/acquisition-royale',
+    iconName: 'dapp',
+  },
+  {
+    name: 'DappRadar',
+    url: 'https://dappradar.com/polygon/games/acquisition-royale',
+    iconName: 'dappRadar',
+  },
+]
+
+export const collaborations: Partner[] = [
+  {
+    name: 'GoodGhosting',
+    url: 'https://medium.com/goodghosting/advent-pool-is-coming-4d67a45fa4a1',
+    iconName: 'goodGhosting',
+  },
+  {
+    name: 'Cometh',
+    url: 'https://medium.com/prepo/acquisition-royale-x-cometh-nft-giveaway-63b125c00b23',
+    iconName: 'cometh',
+  },
+  {
+    name: 'Seascape',
+    url: 'https://medium.com/prepo/acquisition-royale-x-seascape-nft-giveaway-81659c81f7fa',
+    iconName: 'seascape',
+  },
+  {
+    name: 'Evolution Land',
+    url: 'https://medium.com/prepo/acquisition-royale-x-evolution-land-nft-giveaway-94fd10611c09',
+    iconName: 'evolutionLand',
+  },
+  {
+    name: 'Polygon Punks',
+    url: 'https://medium.com/prepo/acquisition-royale-x-polygonpunks-nft-giveaway-46832fdc6fb2',
+    iconName: 'polygonPunks',
+  },
+  // {
+  //   name: 'CrystlFinance',
+  //   url: 'https://www.crystl.finance/',
+  //   iconName: 'crystlFinance',
+  // },
+  // {
+  //   name: 'FirebirdFinance',
+  //   url: 'https://firebird.finance/',
+  //   iconName: 'firebirdFinance',
+  // },
+  // {
+  //   name: 'G4N9',
+  //   url: 'https://cryptog4n9.online/',
+  //   iconName: 'g4n9',
+  // },
+]
