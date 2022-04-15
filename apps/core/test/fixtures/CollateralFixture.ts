826faf0ab7c86f1933e906cf1b12dd6edc514e52
Roan <r0x4xn@protonmail.com>
2022-04-15 11:27:27 +0200

    feat: add core repo test files

---
+++ apps/core/test/fixtures/CollateralFixture.ts
@@ -0,0 +1,33 @@
+import chai from 'chai'
+import { solidity } from 'ethereum-waffle'
+import { ethers, upgrades } from 'hardhat'
+import { Collateral, TestCollateral } from '../../typechain'
+
+chai.use(solidity)
+
+export async function collateralFixture(
+  tokenAddress: string,
+  treasuryAddress: string
+): Promise<Collateral> {
+  const collateral = await ethers.getContractFactory('Collateral')
+  return (await upgrades.deployProxy(collateral, [tokenAddress, treasuryAddress])) as Collateral
+}
+
+export async function testCollateralFixture(
+  tokenAddress: string,
+  treasuryAddress: string
+): Promise<TestCollateral> {
+  const testCollateral = await ethers.getContractFactory('TestCollateral')
+  /**
+   * This is only meant for testing to prevent warnings for use of delegatecall().
+   * OpenZeppelin Upgrades by default does not allow the logic contract to
+   * use delegatecall() since the fear is that the target of the delegatecall()
+   * could call selfdestruct() which would destroy the logic contract, and thus
+   * leave the storage contract pointing to a nonexistent implementation. For
+   * testing purposes, this is not an issue so we silence the warning.
+   */
+  upgrades.silenceWarnings()
+  return (await upgrades.deployProxy(testCollateral, [tokenAddress, treasuryAddress], {
+    unsafeAllow: ['delegatecall'],
+  })) as TestCollateral
+}
---
+++ apps/core/test/fixtures/MockERC20Fixture.ts
@@ -0,0 +1,11 @@
+import chai from 'chai'
+import { solidity } from 'ethereum-waffle'
+import { ethers } from 'hardhat'
+import { MockERC20 } from '../../typechain/MockERC20'
+
+chai.use(solidity)
+
+export async function mockERC20Fixture(tokenName: string, tokenSymbol: string): Promise<MockERC20> {
+  const mockERC20 = await ethers.getContractFactory('MockERC20')
+  return (await mockERC20.deploy(tokenName, tokenSymbol)) as MockERC20
+}
