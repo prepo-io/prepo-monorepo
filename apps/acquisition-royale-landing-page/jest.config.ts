974418dda99244ccccd51848446d6649fcd8e112
Artem Brilev <razdvajonez@mail.ru>
2022-04-18 14:09:20 +0300

    fix: fixed jest, turbo.json

--- apps/acquisition-royale-landing-page/jest.config.ts
+++ apps/acquisition-royale-landing-page/jest.config.ts
@@ -7,8 +7,8 @@ const fromRoot = (d): string => path.join(__dirname, d)
 const config: Config.InitialOptions = {
   ...base,
   roots: [fromRoot('.')],
-  name: 'prepo-website',
-  displayName: 'prePO Website Tests',
+  name: 'acquisition-royale-landing-page',
+  displayName: 'Acquisition Royale Landing Page Tests',
 }
 
 export default config
