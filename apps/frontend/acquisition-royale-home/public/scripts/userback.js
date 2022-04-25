917bf1d207dec4a7894e1153ef78f4c451187642
Man Hau Ling <chrisling.dev@gmail.com>
2022-04-25 11:14:57 +0800

    feat: public folder

---
+++ apps/frontend/acquisition-royale-home/public/scripts/userback.js
@@ -0,0 +1,7 @@
+window.Userback = window.Userback || {};
+Userback.access_token = '32055|65291|F13GSn8v6j6nQFT5r66x4b9Lr';
+(function (d) {
+  var s = d.createElement('script'); s.async = true;
+  s.src = 'https://static.userback.io/widget/v1.js';
+  (d.head || d.body).appendChild(s);
+})(document);
