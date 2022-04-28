83711fab1754e97d4296127631f31640c44db3a6
Roan <85726992+r0x4xn@users.noreply.github.com>
2022-04-28 19:54:05 +0200

    feat: test deployments website (#32)

--- apps/frontend/website/src/pages/_app.tsx
+++ apps/frontend/website/src/pages/_app.tsx
@@ -9,8 +9,6 @@ import 'tailwindcss/tailwind.css'
 import '../styles/default.css'
 import { PANELBEAR_SITE_ID } from '../lib/constants'
 
-// Test second change website
-
 // mobx config
 configure({
   enforceActions: 'observed',
