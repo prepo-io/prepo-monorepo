94c28960d539e711265bcfb11c61b19c271b61d1
Roan <r0x4xn@protonmail.com>
2022-04-28 13:45:14 +0200

    feat: website test change

--- apps/frontend/website/src/pages/_app.tsx
+++ apps/frontend/website/src/pages/_app.tsx
@@ -9,6 +9,8 @@ import 'tailwindcss/tailwind.css'
 import '../styles/default.css'
 import { PANELBEAR_SITE_ID } from '../lib/constants'
 
+// website test change | will remove this line soon
+
 // mobx config
 configure({
   enforceActions: 'observed',
