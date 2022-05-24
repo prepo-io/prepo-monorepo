050da4f37c7371ff841d7d2035bf08e6d4864fc8
Man Hau Ling <chrisling.dev@gmail.com>
2022-05-24 15:06:25 +0800

    fix: default value of useCountdown

--- apps/frontend/acquisition-royale-home/src/hooks/useCountdown.ts
+++ apps/frontend/acquisition-royale-home/src/hooks/useCountdown.ts
@@ -1,16 +1,21 @@
 import { SEC_IN_MS } from '@prepo-io/constants'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useInterval } from 'react-use'
 import { formatPeriod, FormatPeriodOptions } from '../utils/date-utils'
 
 const useCountdown = (deadlineInMs: number, formatOptions?: FormatPeriodOptions): string => {
-  const [period, setPeriod] = useState('')
+  const [period, setPeriod] = useState(formatPeriod(deadlineInMs, formatOptions))
   const handleResetPeriod = useCallback(() => {
     const newResetPeriod = formatPeriod(deadlineInMs, formatOptions)
     setPeriod(newResetPeriod)
   }, [deadlineInMs, formatOptions])
 
+  useEffect(() => {
+    setPeriod(formatPeriod(deadlineInMs, formatOptions))
+  }, [deadlineInMs, formatOptions])
+
   useInterval(handleResetPeriod, SEC_IN_MS)
+
   return period
 }
 
