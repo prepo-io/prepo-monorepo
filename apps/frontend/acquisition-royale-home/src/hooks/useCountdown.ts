ff49763f8e57d8278217a99b5c6ae3c53435a832
Man Hau Ling <chrisling.dev@gmail.com>
2022-05-24 14:42:21 +0800

    feat: hook useCountdown

--- apps/frontend/acquisition-royale-home/src/hooks/useCountdown.ts
+++ apps/frontend/acquisition-royale-home/src/hooks/useCountdown.ts
@@ -1,21 +1,16 @@
 import { SEC_IN_MS } from '@prepo-io/constants'
-import { useCallback, useEffect, useState } from 'react'
+import { useCallback, useState } from 'react'
 import { useInterval } from 'react-use'
 import { formatPeriod, FormatPeriodOptions } from '../utils/date-utils'
 
 const useCountdown = (deadlineInMs: number, formatOptions?: FormatPeriodOptions): string => {
-  const [period, setPeriod] = useState(formatPeriod(deadlineInMs, formatOptions))
+  const [period, setPeriod] = useState('')
   const handleResetPeriod = useCallback(() => {
     const newResetPeriod = formatPeriod(deadlineInMs, formatOptions)
     setPeriod(newResetPeriod)
   }, [deadlineInMs, formatOptions])
 
-  useEffect(() => {
-    setPeriod(formatPeriod(deadlineInMs, formatOptions))
-  }, [deadlineInMs, formatOptions])
-
   useInterval(handleResetPeriod, SEC_IN_MS)
-
   return period
 }
 
