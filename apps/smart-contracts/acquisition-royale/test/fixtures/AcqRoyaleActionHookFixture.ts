402f2b1012f2a0a24183157706c66dd8d2d9bd37
Roan <r0x4xn@protonmail.com>
2022-06-08 14:53:35 +0200

    feat: shutdown ar smart contracts

--- apps/smart-contracts/acquisition-royale/test/fixtures/AcqRoyaleActionHookFixture.ts
+++
@@ -1,7 +0,0 @@
-import { ethers } from 'hardhat'
-import { AcqRoyaleActionHook } from '../../typechain/AcqRoyaleActionHook'
-
-export async function acqRoyaleActionHookFixture(): Promise<AcqRoyaleActionHook> {
-  const Factory = await ethers.getContractFactory('AcqRoyaleActionHook')
-  return (await Factory.deploy()) as AcqRoyaleActionHook
-}
