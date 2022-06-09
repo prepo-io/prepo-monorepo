c646578b3628c9ffd93021dd5db3a053a598564b
Man Hau Ling <chrisling.dev@gmail.com>
2022-06-09 13:48:33 +0800

    feat: testnet deploy script

--- apps/subgraphs/boilerplate/src/mappings/factory.ts
+++ apps/subgraphs/boilerplate/src/mappings/factory.ts
@@ -4,17 +4,17 @@ import { PrePOMarket as PrePOMarketTemplate } from '../types/templates'
 import { MarketCreated } from '../types/templates/PrePOMarket/PrePOMarket'
 
 export function handleMarketAdded(event: MarketAdded): void {
-  const deployedMarket = new DeployedMarket(event.params.market.toString())
-  deployedMarket.longShortHash = event.params.longShortHash.toString()
-  deployedMarket.market = event.params.market.toString()
+  const deployedMarket = new DeployedMarket(event.params.market.toHexString())
+  deployedMarket.longShortHash = event.params.longShortHash.toHexString()
+  deployedMarket.market = event.params.market.toHexString()
   PrePOMarketTemplate.create(event.params.market)
   deployedMarket.save()
 }
 
 export function handleMarketCreated(event: MarketCreated): void {
-  const market = new Market(event.address.toString())
-  market.longToken = event.params.longToken.toString()
-  market.shortToken = event.params.shortToken.toString()
+  const market = new Market(event.address.toHexString())
+  market.longToken = event.params.longToken.toHexString()
+  market.shortToken = event.params.shortToken.toHexString()
   market.ceilingLongPrice = event.params.ceilingLongPrice
   market.ceilingValuation = event.params.ceilingValuation
   market.expiryTime = event.params.expiryTime
