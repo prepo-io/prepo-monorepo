59abab3e84f378baeb844245859732272d789eb4
Man Hau Ling <chrisling.dev@gmail.com>
2022-06-09 11:32:37 +0800

    feat: init project with dependencies, eslint and tsconfig

---
+++ apps/subgraphs/boilerplate/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  ...require('config/eslint-server'),
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+  },
+  rules: {
+    ...require('config/eslint-server').rules,
+    '@typescript-eslint/no-unused-expressions': 'off',
+  },
+  // Removes the jest configuration since we are using chai here
+  overrides: [],
+}
