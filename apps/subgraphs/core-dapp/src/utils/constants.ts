b5f2d361d84572c99a6433c1caee79220246a581
Chris Ling <81092286+chrisling-dev@users.noreply.github.com>
2022-06-17 11:33:49 +0800

    fix: graphql schema comment (#150)
    * fix: graphql schema following  v0.12.3
    
    * feat: subgraph track collateral transfer (#151)
    
    * feat(subgraph): add collateral token transfer handler to subgraph.yaml
    
    * feat(subgraph): transaction entity
    
    * feat: track transfer of collateral tokens
    
    * feat: subgraph track long short token transfer (#152)
    
    * feat: track long short token transfer
    
    * feat: track swap event of uniswap pool (#153)

--- apps/subgraphs/core-dapp/src/utils/constants.ts
+++ apps/subgraphs/core-dapp/src/utils/constants.ts
@@ -8,3 +8,16 @@ export const DEFAULT_LONG_SHORT_DECIMALS = 18
 export const ONE_BI = BigInt.fromI32(1)
 export const ZERO_BI = BigInt.fromI32(0)
 export const ZERO_BD = BigDecimal.fromString('0')
+
+// ... transactions ...
+// wasm/assemblyscript can't compile objects nor enum
+// so we must set these separately
+
+export const EVENTS_TRANSFER = 'TRANSFER'
+export const EVENTS_SWAP = 'SWAP'
+
+export const ACTIONS_SEND = 'SEND'
+export const ACTIONS_RECEIVE = 'RECEIVE'
+export const ACTIONS_OPEN = 'OPEN'
+export const ACTIONS_CLOSE = 'CLOSE'
+export const ACTIONS_POOL = 'POOL'
