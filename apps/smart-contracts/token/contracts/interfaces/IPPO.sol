2e3f732b675146c615adc16a8ebcc416c111ad0c
Kumar Nilay <nilay@prepo.io>
2022-07-15 22:56:57 +0530

    feat: adding interfaces for PPO and TransferHook

---
+++ apps/smart-contracts/token/contracts/interfaces/IPPO.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity =0.8.7;
+
+import "./ITransferHook.sol";
+
+//TODO: add all natspecs at the end
+interface IPPO {
+    function setTransferHook(ITransferHook newTransferHook) external;
+
+    function mint(uint256 amount) external;
+
+    function burn(uint256 amount) external;
+
+    function burnFrom(address account, uint256 amount) external;
+}
