6c1e5d7b04417dfc227ffb20ae18125fcd23f30e
Kumar Nilay <nilay@prepo.io>
2022-07-16 20:19:32 +0530

    feat: adding interface for block list for ppo contract (#207)
    * feat: adding interface for block list for ppo contract
    
    * feat: cr#207 rev1: included suggestion to set and reset blocklist

---
+++ apps/smart-contracts/token/contracts/interfaces/IBlockList.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity =0.8.7;
+
+//TODO: add all natspecs at the end
+interface IBlocklist {
+  function set(address[] calldata accounts, bool[] calldata blocked) external;
+
+  function reset(address[] calldata blockedAccounts) external;
+}
