98aee8e87d21720f776753aedec2638b370d77d9
ramenforbreakfast <ramenforbreakfast@protonmail.com>
2022-07-15 19:56:25 -0700

    chore: remove PPO and IOUPPO

--- apps/smart-contracts/token/test/fixtures/IOUPPOFixtures.ts
+++
@@ -1,7 +0,0 @@
-import { ethers, upgrades } from 'hardhat'
-import { IOUPPO } from '../../types/generated'
-
-export async function iouPPOFixture(iouPPOOwner: string): Promise<IOUPPO> {
-  const Factory = await ethers.getContractFactory('IOUPPO')
-  return (await upgrades.deployProxy(Factory, [iouPPOOwner])) as unknown as IOUPPO
-}
