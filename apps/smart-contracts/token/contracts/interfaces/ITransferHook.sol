0a8418346bf402f91eebd5f309a9dec0adbd9718
Kumar Nilay <nilay@prepo.io>
2022-07-16 16:13:24 +0530

    feat: adding interfaces for PPO and TransferHook (#205)
    * feat: adding interfaces for PPO and TransferHook
    
    * feat: ran linter

---
+++ apps/smart-contracts/token/contracts/interfaces/ITransferHook.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity =0.8.7;
+
+//TODO: add all natspecs at the end
+interface ITransferHook {
+  function hook(
+    address from,
+    address to,
+    uint256 amount
+  ) external;
+}
