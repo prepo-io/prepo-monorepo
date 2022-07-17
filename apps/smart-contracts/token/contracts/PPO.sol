f53c667c37ecf1bc6e687a349e3a5a84ea030190
Kumar Nilay <nilay@prepo.io>
2022-07-17 15:17:11 +0530

    feat: cr#212: resolved nits

--- apps/smart-contracts/token/contracts/PPO.sol
+++ apps/smart-contracts/token/contracts/PPO.sol
@@ -2,17 +2,16 @@
 pragma solidity =0.8.7;
 
 import "./interfaces/IPPO.sol";
-import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "prepo-shared-contracts/contracts/SafeOwnableUpgradeable.sol";
 
-contract PPO is IPPO, ReentrancyGuardUpgradeable, SafeOwnableUpgradeable {
+contract PPO is IPPO, SafeOwnableUpgradeable {
   function initialize(address _nominatedOwner) public initializer {}
 
   function setTransferHook(ITransferHook _newTransferHook) external override onlyOwner {}
 
-  function mint(uint256 _amount) external override onlyOwner {}
+  function mint(uint256 _amount) external override {}
 
-  function burn(uint256 _amount) external override onlyOwner {}
+  function burn(uint256 _amount) external override {}
 
   function burnFrom(address _account, uint256 _amount) external override onlyOwner {}
 }
--- apps/smart-contracts/token/test/PPO.test.ts
+++ apps/smart-contracts/token/test/PPO.test.ts
@@ -1,7 +1,4 @@
 import chai from 'chai'
-import { solidity } from 'ethereum-waffle'
 import { ppoFixture } from './fixtures/PPOFixtures'
-
-chai.use(solidity)
 
 describe('=> PPO', () => {})
