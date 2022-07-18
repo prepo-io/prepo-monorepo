20f848e57dde23f7270bff4d0530d03d7042e6a3
ramenforbreakfast <ramenforbreakfast@protonmail.com>
2022-07-18 12:48:09 -0700

    chore: remove allowlist and blocklist (#223)
    * chore: remove allowlist and blocklist
    
    * feat: add IAccountList (#224)
    
    * feat: add IAccesslist
    
    * fix: change name to AccountList and use include naming scheme
    
    * fix: capitalize filename and change to isIncludedAccount

--- apps/smart-contracts/token/test/fixtures/BlocklistFixtures.ts
+++
@@ -1,7 +0,0 @@
-import { ethers } from 'hardhat'
-import { Blocklist } from '../../types/generated'
-
-export async function blocklistFixture(nominatedOwnerAddress: string): Promise<Blocklist> {
-  const Factory = await ethers.getContractFactory('Blocklist')
-  return (await Factory.deploy(nominatedOwnerAddress)) as unknown as Blocklist
-}
