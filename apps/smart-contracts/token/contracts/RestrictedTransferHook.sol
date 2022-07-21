2e2e05d12e9c4a5e4c22c7d8e957a63067d8aba6
Kumar Nilay <nilay@prepo.io>
2022-07-21 17:39:41 +0530

    feat: changed inheritance order of transfer hooks

--- apps/smart-contracts/token/contracts/RestrictedTransferHook.sol
+++ apps/smart-contracts/token/contracts/RestrictedTransferHook.sol
@@ -3,6 +3,7 @@ pragma solidity =0.8.7;
 
 import "./interfaces/IRestrictedTransferHook.sol";
 import "./interfaces/IAccountList.sol";
+import "./BlocklistTransferHook.sol";
 import "prepo-shared-contracts/contracts/SafeOwnable.sol";
 
 /**
@@ -11,19 +12,13 @@ import "prepo-shared-contracts/contracts/SafeOwnable.sol";
   TODO: Call super.hook(...) to check for blocklist in `hook()`.
 */
 
-contract RestrictedTransferHook is IRestrictedTransferHook, SafeOwnable {
+contract RestrictedTransferHook is IRestrictedTransferHook, SafeOwnable, BlocklistTransferHook {
   //TODO: Extract this to a shared contract to reduce duplication.
   address private _token;
-  IAccountList private _blocklist;
   IAccountList private _sourceAllowlist;
   IAccountList private _destinationAllowlist;
 
-  modifier onlyToken() {
-    require(msg.sender == _token, "msg.sender != token");
-    _;
-  }
-
-  constructor(address _nominatedOwner) {
+  constructor(address _nominatedOwner) BlocklistTransferHook(_nominatedOwner) {
     transferOwnership(_nominatedOwner);
   }
 
@@ -31,19 +26,10 @@ contract RestrictedTransferHook is IRestrictedTransferHook, SafeOwnable {
     address _from,
     address _to,
     uint256 _amount
-  ) external override onlyToken {
-    require(!_blocklist.isIncluded(_from), "Sender blocked");
-    require(!_blocklist.isIncluded(_to), "Recipient blocked");
+  ) public virtual override(BlocklistTransferHook, ITransferHook) onlyToken {
+    super.hook(_from, _to, _amount);
     if (_sourceAllowlist.isIncluded(_from)) return;
     require(_destinationAllowlist.isIncluded(_to), "Destination not allowed");
-  }
-
-  function setToken(address _newToken) external onlyOwner {
-    _token = _newToken;
-  }
-
-  function setBlocklist(IAccountList _newBlockedAccounts) external override onlyOwner {
-    _blocklist = _newBlockedAccounts;
   }
 
   function setSourceAllowlist(IAccountList _newAllowedSources) external override onlyOwner {
@@ -56,14 +42,6 @@ contract RestrictedTransferHook is IRestrictedTransferHook, SafeOwnable {
     onlyOwner
   {
     _destinationAllowlist = _newAllowedDestinations;
-  }
-
-  function getToken() external view returns (address) {
-    return _token;
-  }
-
-  function getBlocklist() external view override returns (IAccountList) {
-    return _blocklist;
   }
 
   function getSourceAllowlist() external view override returns (IAccountList) {
